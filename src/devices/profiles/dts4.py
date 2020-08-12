eb2cb992052060c4fb1b1ca1fa4c9f7d529ed2a6
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-08-12 17:28:52 -0400

    Implemented custom recieve buffer for DTS-4 profile

--- src/devices/profiles/dts4.py
+++ src/devices/profiles/dts4.py
@@ -3,14 +3,48 @@ from qt import *
 import logging
 
 
+
+class NewlineBuffer:
+    def __init__(self):
+        self.buffer = ""
+        self.depth = 0
+
+    def reset(self):
+        self.buffer = ""
+        self.depth = 0
+
+    def insert_char(self, c):
+        print(c)
+        self.buffer += c
+
+    def completed(self):
+        if '\n' in self.buffer:
+            print('got newline')
+            return True
+
+        return False
+
+
+class Signals(QObject):
+    antenna = Signal(int)
+
+
 class DTS4(SerialDevice):
     profile_name = "DTS-4"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.msg = NewlineBuffer()
+        self.signals = Signals()
 
     def __init__(self, port=None, baud=115200, device=None):
         super().__init__(port=port, baud=baud, device=device)
 
     def select_antenna(self, antenna):
-        self.send(f"\r\nant set {antenna}\r\n")
+        self.send(f"ant set {antenna}\r\n")
 
     def read_antenna(self):
-        self.send("\r\nant read\r\n")
+        self.send("ant read\r\n")
+    
+    def recieve(self, string):
+        print(string)
