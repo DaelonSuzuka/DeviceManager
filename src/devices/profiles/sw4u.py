7c1d4e6e3b3822146a0a0ab9005d7231ecf15c4e
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-08-28 14:42:31 -0400

    Removed unused labels from SW-4U control widget

--- src/devices/profiles/sw4u.py
+++ src/devices/profiles/sw4u.py
@@ -121,15 +121,9 @@ class RFPanel(QGroupBox):
         
         # widgets
         self.frequency = QLabel("?")
-        self.forward = QLabel("?")
-        self.reverse = QLabel("?")
 
         # layout
         grid = QGridLayout(self)
-        grid.addWidget(QLabel("Forward:"), 0, 0)
-        grid.addWidget(self.forward, 0, 1)
-        grid.addWidget(QLabel("Reverse:"), 0, 2)
-        grid.addWidget(self.reverse, 0, 3)
         grid.addWidget(QLabel("Frequency:"), 0, 4)
         grid.addWidget(self.frequency, 0, 5)
 
