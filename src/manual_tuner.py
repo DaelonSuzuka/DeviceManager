9d702d8035d8910cf408e16394e264bf2d075ad4
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-09-23 17:33:43 -0400

    Added protottype quadrant tuning controls

--- src/manual_tuner.py
+++ src/manual_tuner.py
@@ -4,6 +4,44 @@ from device_widgets import *
 from servitor import KeyButton, RadioInfo, MeterInfo
 
 
+class TunerButtons(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setStyleSheet("""
+            QPushButton { 
+                max-width: 2000px; 
+                max-height: 2000px; 
+                min-width: 200px; 
+                min-height: 200px; 
+            } 
+        """)
+
+        self.upper_left = QPushButton(checkable=True)
+        self.upper_left.setIconSize(QSize(100, 100))
+        self.upper_left.setIcon(QIcon('img/upper_left.png'))
+        self.upper_right = QPushButton(checkable=True)
+        self.upper_right.setIconSize(QSize(100, 100))
+        self.upper_right.setIcon(QIcon('img/upper_right.png'))
+        self.bottom_left = QPushButton(checkable=True)
+        self.bottom_left.setIconSize(QSize(100, 100))
+        self.bottom_left.setIcon(QIcon('img/bottom_left.png'))
+        self.bottom_right = QPushButton(checkable=True)
+        self.bottom_right.setIconSize(QSize(100, 100))
+        self.bottom_right.setIcon(QIcon('img/bottom_right.png'))
+
+        self.buttons = QButtonGroup()
+        self.buttons.addButton(self.upper_left)
+        self.buttons.addButton(self.upper_right)
+        self.buttons.addButton(self.bottom_left)
+        self.buttons.addButton(self.bottom_right)
+
+        with CGridLayout(self) as layout:
+            layout.add(self.upper_left, 0, 0)
+            layout.add(self.upper_right, 0, 1)
+            layout.add(self.bottom_left, 1, 0)
+            layout.add(self.bottom_right, 1, 1)
+
+
 class ManualTuner(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
