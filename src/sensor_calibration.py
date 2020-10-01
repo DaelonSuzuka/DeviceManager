9c2428186f8f9bb6eb82b2941507359e2d111f70
DaelonSuzuka <david@ldgelectronics.com>
2020-10-01 18:51:47 -0400

    Calibration data collection is now functional

--- src/sensor_calibration.py
+++ src/sensor_calibration.py
@@ -3,16 +3,24 @@ from device_manager import DeviceManager
 from device_widgets import *
 from servitor import RadioInfo, MeterInfo
 from dataclasses import dataclass
+from collections import deque  
 
 
 @dataclass
 class Point:
     freq: str
     power: str
+
+
+@dataclass
+class Result:
+    freq: str = ''
+    power: str = ''
+
     meter_fwd: float = 0
-    # meter_rev: float = 0
-    # meter_swr: float = 0
-    sensor_fwd: float = 0
+    meter_rev: float = 0
+    meter_swr: float = 0
+    # sensor_fwd: float = 0
     # sensor_rev: float = 0
     # sensor_swr: float = 0
 
@@ -37,12 +45,46 @@ class CalibrationWorker(QObject):
 
         self.points = []
         self.current_point = 0
+        self.results = []
+        self.data = {
+            'm_fwd': deque(maxlen=10),
+            'm_rev': deque(maxlen=10),
+            'm_swr': deque(maxlen=10),
+            # 's_fwd': deque(maxlen=10),
+            # 's_rev': deque(maxlen=10),
+            # 's_swr': deque(maxlen=10),
+        }
+
+    def clear_data(self):
+        for _, queue in self.data.items():
+            queue.clear()
+
+    def data_is_ready(self) -> bool:
+        for _, queue in self.data.items():
+            if len(queue) != 10:
+                return False
+
+        # if len(self.data['m_fwd']) != 10:
+        #     return False
+        return True
+
+    def calculate_result(self) -> Result:
+        result = Result()
+        result.freq = self.points[self.current_point].freq
+        result.power = self.points[self.current_point].power
+        result.meter_fwd = sum(self.data['m_fwd']) / 10
+        result.meter_rev = sum(self.data['m_rev']) / 10
+        result.meter_swr = sum(self.data['m_swr']) / 10
+
+        return result
 
     @Slot()
     def start(self, script):
-        self.timer.start(200)
+        self.timer.start(50)
 
         self.points = []
+        self.results = []
+
         for freq in script['freqs']:
             for power in script['powers']:
                 self.points.append(Point(freq, power))
@@ -57,12 +99,18 @@ class CalibrationWorker(QObject):
                 self.radio = device
             if device.profile_name == 'Alpha4510A':
                 self.meter = device
-                self.meter.signals.forward.connect(self.meter_forward)
+                self.meter.signals.forward.connect(lambda x: self.data['m_fwd'].append(float(x)))
+                self.meter.signals.reverse.connect(lambda x: self.data['m_rev'].append(float(x)))
+                self.meter.signals.swr.connect(lambda x: self.data['m_swr'].append(float(x)))
             if device.profile_name == 'RFSensor':
                 self.sensor = device
                 self.sensor.signals.forward.connect(self.sensor_forward)
 
         self.started.emit()
+        
+        self.radio.set_vfoA_frequency(int(self.points[self.current_point].freq))
+        self.radio.set_power_level(int(self.points[self.current_point].power))
+        self.radio.key()
 
     def meter_forward(self, forward):
         self.points[self.current_point].meter_fwd = forward
@@ -74,6 +122,9 @@ class CalibrationWorker(QObject):
     def stop(self):
         self.timer.stop()
 
+        if self.radio:
+            self.radio.unkey()
+
         self.switch = None
         self.meter = None
         self.radio = None
@@ -84,20 +135,26 @@ class CalibrationWorker(QObject):
     def update(self):
         self.updated.emit(self.current_point)
 
-        if self.current_point == len(self.points):
+        if self.current_point == len(self.points) - 1:
             self.stop()
             results = ''
-            for p in self.points:
-                results += str(p) + '\n'
+            for r in self.results:
+                results += str(r) + '\n'
             self.finished.emit(results)
             return
 
-        self.radio.set_vfoA_frequency(int(self.points[self.current_point].freq))
-        self.radio.set_power_level(int(self.points[self.current_point].power))
+        if self.data_is_ready():
+            self.results.append(self.calculate_result())
+            self.clear_data()
+            self.current_point += 1
 
-        self.current_point += 1
+            self.radio.unkey()
+            self.radio.set_vfoA_frequency(int(self.points[self.current_point].freq))
+            self.radio.set_power_level(int(self.points[self.current_point].power))
+            self.radio.key()
 
 
+@DeviceManager.subscribe
 class CalibrationWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
@@ -114,12 +171,12 @@ class CalibrationWidget(QWidget):
                 "03500000",
                 "07000000",
                 "10100000",
-                # "14000000",
-                # "18068000",
-                # "21000000",
-                # "24890000",
-                # "28000000",
-                # "50000000",
+                "14000000",
+                "18068000",
+                "21000000",
+                "24890000",
+                "28000000",
+                "50000000",
             ],
             'powers': [
                 "005",
@@ -128,14 +185,14 @@ class CalibrationWidget(QWidget):
                 "020",
                 "025",
                 "030",
-                # "035",
-                # "040",
-                # "050",
-                # "060",
-                # "070",
-                # "080",
-                # "090",
-                # "100",
+                "035",
+                "040",
+                "050",
+                "060",
+                "070",
+                "080",
+                "090",
+                "100",
             ]
         }
         
@@ -146,10 +203,12 @@ class CalibrationWidget(QWidget):
         self.worker.started.connect(self.worker_started)
         self.worker.stopped.connect(self.worker_stopped)
 
-        self.start = QPushButton('start', clicked=self.start_worker)
-        self.stop = QPushButton('stop', clicked=self.stop_worker, enabled=False)
+        self.start = QPushButton('Start', clicked=self.start_worker)
+        self.stop = QPushButton('Stop', clicked=self.stop_worker, enabled=False)
         self.progress = QProgressBar()
         self.worker.updated.connect(self.progress.setValue)
+
+        self.target = QComboBox()
 
         self.results = QTextEdit('')
         self.worker.finished.connect(self.results.setText)
@@ -167,8 +226,14 @@ class CalibrationWidget(QWidget):
                 with CHBoxLayout(vbox) as hbox:
                     hbox.add(self.start)
                     hbox.add(self.stop)
+                    hbox.add(QLabel(), 1)
+                    hbox.add(QLabel('Target:'))
+                    hbox.add(self.target, 1)
                 vbox.add(self.progress)
                 vbox.add(self.results, 1)
+
+    def device_added(self, device):
+        self.target.addItem(device.title)
 
     def start_worker(self):
         self.worker.start(self.script)
