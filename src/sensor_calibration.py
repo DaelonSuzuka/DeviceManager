ed274339be4d3e5f72c734dd2faf28259f292b8f
DaelonSuzuka <david@ldgelectronics.com>
2020-10-01 21:34:24 -0400

    Enabled RF Sensor in calibration module

--- src/sensor_calibration.py
+++ src/sensor_calibration.py
@@ -20,9 +20,9 @@ class Result:
     meter_fwd: float = 0
     meter_rev: float = 0
     meter_swr: float = 0
-    # sensor_fwd: float = 0
-    # sensor_rev: float = 0
-    # sensor_swr: float = 0
+    sensor_fwd: float = 0
+    sensor_rev: float = 0
+    sensor_swr: float = 0
 
 
 @DeviceManager.subscribe
@@ -30,7 +30,7 @@ class CalibrationWorker(QObject):
     started = Signal()
     stopped = Signal()
     updated = Signal(int)
-    finished = Signal(str)
+    finished = Signal(list)
 
     def __init__(self):
         super().__init__()
@@ -50,9 +50,9 @@ class CalibrationWorker(QObject):
             'm_fwd': deque(maxlen=10),
             'm_rev': deque(maxlen=10),
             'm_swr': deque(maxlen=10),
-            # 's_fwd': deque(maxlen=10),
-            # 's_rev': deque(maxlen=10),
-            # 's_swr': deque(maxlen=10),
+            's_fwd': deque(maxlen=10),
+            's_rev': deque(maxlen=10),
+            's_swr': deque(maxlen=10),
         }
 
     def clear_data(self):
@@ -63,9 +63,6 @@ class CalibrationWorker(QObject):
         for _, queue in self.data.items():
             if len(queue) != 10:
                 return False
-
-        # if len(self.data['m_fwd']) != 10:
-        #     return False
         return True
 
     def calculate_result(self) -> Result:
@@ -75,6 +72,9 @@ class CalibrationWorker(QObject):
         result.meter_fwd = sum(self.data['m_fwd']) / 10
         result.meter_rev = sum(self.data['m_rev']) / 10
         result.meter_swr = sum(self.data['m_swr']) / 10
+        result.sensor_fwd = sum(self.data['s_fwd']) / 10
+        result.sensor_rev = sum(self.data['s_rev']) / 10
+        result.sensor_swr = sum(self.data['s_swr']) / 10
 
         return result
 
@@ -104,7 +104,9 @@ class CalibrationWorker(QObject):
                 self.meter.signals.swr.connect(lambda x: self.data['m_swr'].append(float(x)))
             if device.profile_name == 'RFSensor':
                 self.sensor = device
-                self.sensor.signals.forward.connect(self.sensor_forward)
+                self.sensor.signals.forward.connect(lambda x: self.data['s_fwd'].append(float(x)))
+                self.sensor.signals.reverse.connect(lambda x: self.data['s_rev'].append(float(x)))
+                self.sensor.signals.match_quality.connect(lambda x: self.data['s_swr'].append(float(x)))
 
         self.started.emit()
         
@@ -135,23 +137,20 @@ class CalibrationWorker(QObject):
     def update(self):
         self.updated.emit(self.current_point)
 
-        if self.current_point == len(self.points) - 1:
-            self.stop()
-            results = ''
-            for r in self.results:
-                results += str(r) + '\n'
-            self.finished.emit(results)
-            return
-
         if self.data_is_ready():
             self.results.append(self.calculate_result())
-            self.clear_data()
             self.current_point += 1
 
             self.radio.unkey()
             self.radio.set_vfoA_frequency(int(self.points[self.current_point].freq))
             self.radio.set_power_level(int(self.points[self.current_point].power))
             self.radio.key()
+
+            self.clear_data()
+
+        if self.current_point == len(self.points) - 1:
+            self.stop()
+            self.finished.emit(self.results)
 
 
 @DeviceManager.subscribe
@@ -167,16 +166,16 @@ class CalibrationWidget(QWidget):
         """)
         self.script = {
             'freqs': [
-                "01800000",
-                "03500000",
-                "07000000",
-                "10100000",
+                # "01800000",
+                # "03500000",
+                # "07000000",
+                # "10100000",
                 "14000000",
-                "18068000",
-                "21000000",
-                "24890000",
-                "28000000",
-                "50000000",
+                # "18068000",
+                # "21000000",
+                # "24890000",
+                # "28000000",
+                # "50000000",
             ],
             'powers': [
                 "005",
@@ -188,11 +187,11 @@ class CalibrationWidget(QWidget):
                 "035",
                 "040",
                 "050",
-                "060",
-                "070",
-                "080",
-                "090",
-                "100",
+                # "060",
+                # "070",
+                # "080",
+                # "090",
+                # "100",
             ]
         }
         
@@ -203,15 +202,16 @@ class CalibrationWidget(QWidget):
         self.worker.started.connect(self.worker_started)
         self.worker.stopped.connect(self.worker_stopped)
 
-        self.start = QPushButton('Start', clicked=self.start_worker)
-        self.stop = QPushButton('Stop', clicked=self.stop_worker, enabled=False)
+        self.start = QPushButton('Start', clicked=lambda: self.worker.start(self.script))
+        self.stop = QPushButton('Stop', clicked=self.worker.stop, enabled=False)
         self.progress = QProgressBar()
         self.worker.updated.connect(self.progress.setValue)
 
         self.target = QComboBox()
+        self.master = QComboBox()
 
         self.results = QTextEdit('')
-        self.worker.finished.connect(self.results.setText)
+        self.worker.finished.connect(self.worker_finished)
 
         with CHBoxLayout(self) as layout:
             with CVBoxLayout(layout) as vbox:
@@ -224,25 +224,22 @@ class CalibrationWidget(QWidget):
 
             with CVBoxLayout(layout, 1) as vbox:
                 with CHBoxLayout(vbox) as hbox:
-                    hbox.add(self.start)
-                    hbox.add(self.stop)
-                    hbox.add(QLabel(), 1)
+                    hbox.add(QLabel('Master:'))
+                    hbox.add(self.master, 1)
                     hbox.add(QLabel('Target:'))
                     hbox.add(self.target, 1)
+                    hbox.add(QLabel(), 1)
+                    hbox.add(self.start)
+                    hbox.add(self.stop)
                 vbox.add(self.progress)
                 vbox.add(self.results, 1)
 
     def device_added(self, device):
         self.target.addItem(device.title)
-
-    def start_worker(self):
-        self.worker.start(self.script)
-        self.progress.setMaximum(len(self.script['freqs']) * len(self.script['powers']))
-
-    def stop_worker(self):
-        self.worker.stop()
+        self.master.addItem(device.title)
 
     def worker_started(self):
+        self.progress.setMaximum(len(self.script['freqs']) * len(self.script['powers']))
         self.progress.setValue(0)
         self.start.setEnabled(False)
         self.stop.setEnabled(True)
@@ -251,3 +248,9 @@ class CalibrationWidget(QWidget):
         self.progress.setValue(0)
         self.start.setEnabled(True)
         self.stop.setEnabled(False)
+
+    def worker_finished(self, results):
+        s = ''
+        for r in results:
+            s += str(r) + '\n'
+        self.results.setText(s)
