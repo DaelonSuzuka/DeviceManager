e51596081c5b12c714efabc603bd5a9b6fba8360
DaelonSuzuka <daelonsuzuka@gmail.com>
2020-10-12 16:00:11 -0400

    Added CalibrationTarget profile

---
+++ src/plugins/devices/calibration_target.py
@@ -0,0 +1,38 @@
+from devices import SerialDevice, CommonMessagesMixin
+from qt import *
+
+
+class Signals(QObject):
+    forward = Signal(float)
+    reverse = Signal(float)
+    forward_watts = Signal(float)
+    reverse_watts = Signal(float)
+    match_quality = Signal(float)
+    swr = Signal(float)
+    frequency = Signal(int)
+    phase = Signal(int)
+
+    @property
+    def message_tree(self):
+        return {
+            "update": {
+                "forward_volts": self.forward.emit,
+                "reverse_volts": self.reverse.emit,
+                "match_quality": self.match_quality.emit,
+                "forward": self.forward_watts.emit,
+                "reverse": self.reverse_watts.emit,
+                "swr": self.swr.emit,
+                "frequency": self.frequency.emit,
+                "phase": self.phase.emit,
+            }
+        }
+
+
+class CalibrationTarget(CommonMessagesMixin, SerialDevice):
+    profile_name = "CalibrationTarget"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.signals = Signals()
+        self.message_tree.merge(self.signals.message_tree)
+        self.message_tree.merge(self.common_message_tree)
