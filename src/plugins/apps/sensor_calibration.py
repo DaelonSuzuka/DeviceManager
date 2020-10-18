7929962b4842fdb0f0c799033bb2daae8e284a64
DaelonSuzuka <david@ldgelectronics.com>
2020-10-18 18:11:39 -0400

    Added preconfigured graph profiles

--- src/plugins/apps/sensor_calibration.py
+++ src/plugins/apps/sensor_calibration.py
@@ -6,7 +6,7 @@ from collections import deque
 import numpy as np
 import json
 import pyqtgraph as pg
-from .test_data2 import test_data
+from .test_data import test_data
 
 
 freqs = ["01800000", "03500000", "07000000", "10100000", "14000000", "18068000", "21000000", "24890000", "28000000", "50000000", ]
@@ -17,7 +17,28 @@ data_fields = [
     't_fwd_watts', 't_rev_watts', 't_swr', 't_freq',
 ]
 
-data_field_names = {
+
+class bidict(dict):
+    def __init__(self, *args, **kwargs):
+        super(bidict, self).__init__(*args, **kwargs)
+        self.inverse = {}
+        for key, value in self.items():
+            self.inverse.setdefault(value,[]).append(key) 
+
+    def __setitem__(self, key, value):
+        if key in self:
+            self.inverse[self[key]].remove(key) 
+        super(bidict, self).__setitem__(key, value)
+        self.inverse.setdefault(value,[]).append(key)        
+
+    def __delitem__(self, key):
+        self.inverse.setdefault(self[key],[]).remove(key)
+        if self[key] in self.inverse and not self.inverse[self[key]]: 
+            del self.inverse[self[key]]
+        super(bidict, self).__delitem__(key)
+
+
+data_field_names = bidict({
     'm_fwd': 'Meter: Forward Watts',
     'm_rev': 'Meter: Reverse Watts',
     'm_swr': 'Meter: SWR',
@@ -30,7 +51,7 @@ data_field_names = {
     't_rev_watts': 'Target: Reverse Watts',
     't_swr': 'Target: SWR',
     't_freq': 'Target: Frequency',
-}
+})
 
 
 @dataclass
@@ -87,7 +108,7 @@ class DataQueue:
 
 @DeviceManager.subscribe
 class CalibrationWorker(QObject):
-    started = Signal()
+    started = Signal(int)
     stopped = Signal()
     updated = Signal(int)
     finished = Signal(list)
@@ -149,7 +170,7 @@ class CalibrationWorker(QObject):
                 self.target.signals.swr.connect(lambda x: self.data['t_swr'].append(float(x)))
                 self.target.signals.frequency.connect(lambda x: self.data['t_freq'].append(float(x)))
 
-        self.started.emit()
+        self.started.emit(len(self.points))
         
         self.target.send('calibrate\n')
         self.switch.set_antenna(1)
@@ -209,14 +230,21 @@ class DataSelector(QWidget):
         changed = self.selectionChanged
         
         self.on = PersistentCheckBox(f'{name}_on', changed=changed)
+        self.points = PersistentCheckBox(f'{name}_points', changed=changed)
+        self.line = PersistentCheckBox(f'{name}_line', changed=changed)
         self.freqs = PersistentListWidget(f'{name}_freqs', items=['none']+freqs, default=['none'], selectionMode=QAbstractItemView.ExtendedSelection, changed=changed)
-        self.x = PersistentComboBox(f'{name}_x', items=data_fields, changed=changed)
-        self.y = PersistentComboBox(f'{name}_y', items=data_fields, changed=changed)
+        field_names = [data_field_names[f] for f in data_fields]
+        self.x = PersistentComboBox(f'{name}_x', items=field_names, changed=changed)
+        self.y = PersistentComboBox(f'{name}_y', items=field_names, changed=changed)
 
         with CVBoxLayout(self) as vbox:
             with CHBoxLayout(vbox) as hbox:
                 hbox.add(QLabel(name))
                 hbox.add(QLabel(), 1)
+                hbox.add(QLabel('Pts:'))
+                hbox.add(self.points)
+                hbox.add(QLabel('Line:'))
+                hbox.add(self.line)
                 hbox.add(QLabel('On:'))
                 hbox.add(self.on)
             with CHBoxLayout(vbox) as hbox:
@@ -228,7 +256,14 @@ class DataSelector(QWidget):
 
     def get_params(self):
         if self.on.checkState() and self.x.currentText() != self.y.currentText():
-            return (self.x.currentText(), self.y.currentText(), self.freqs.selected_items())
+            return {
+                'x': data_field_names.inverse[self.x.currentText()][0], 
+                'y': data_field_names.inverse[self.y.currentText()][0], 
+                'on': self.on.checkState(), 
+                'points': self.points.checkState(), 
+                'line': self.line.checkState(), 
+                'freqs': self.freqs.selected_items(),
+            }
         return
 
 
@@ -237,7 +272,7 @@ class GraphTab(QWidget):
         super().__init__(parent=parent)
         
         self.plot_layout = pg.GraphicsLayoutWidget()
-        self.plots = [DataSelector(f'Plot {i}', self) for i in range(1, 7)]
+        self.custom_plots = [DataSelector(f'Custom Plot {i}', self) for i in range(1, 5)]
 
         def normalize(x):
             x = np.asarray(x)
@@ -249,54 +284,147 @@ class GraphTab(QWidget):
             'graph_freqs', items=freqs, selectionMode=QAbstractItemView.ExtendedSelection, changed=self.draw_plots)
         self.freq_tabs = PersistentTabWidget('graph_tabs')
         self.freq_tabs.addTab(self.freqs, 'all')
-        for plot in self.plots:
-            self.freq_tabs.addTab(plot.freqs, plot.name[5:])
+        for plot in self.custom_plots:
+            self.freq_tabs.addTab(plot.freqs, plot.name[11:])
         self.freq_tabs.restore_state()
 
-        with CHBoxLayout(self) as hbox:
-            with CVBoxLayout(hbox) as vbox:
-                vbox.add(QLabel('Freqs'))
+        self.fwd_v = PersistentCheckBox('graph_fwd_v', changed=self.draw_plots)
+        self.fwd_w = PersistentCheckBox('graph_fwd_w', changed=self.draw_plots)
+
+        with CHBoxLayout(self) as layout:
+            with CVBoxLayout(layout) as vbox:
+                vbox.add(QLabel('Reference Plots:'))
+                with CHBoxLayout(vbox) as hbox:
+                    hbox.add(QLabel('Forward Volts:'))
+                    hbox.add(self.fwd_v)
+                with CHBoxLayout(vbox) as hbox:
+                    hbox.add(QLabel('Forward Watts:'))
+                    hbox.add(self.fwd_w)
+                vbox.add(HLine())
+                vbox.add(QLabel('Custom Plot Freqs:'))
                 vbox.add(self.freq_tabs, 1)
-                vbox.add(self.plots)
-            hbox.add(self.plot_layout, 1)
+                vbox.add(self.custom_plots)
+            layout.add(self.plot_layout, 1)
 
         self.data = {}
         self.set_data(test_data)
 
-        for plot in self.plots:
+        for plot in self.custom_plots:
             plot.selectionChanged.connect(self.draw_plots)
+
+    def get_color_map(self, freqs):
+        x = np.asarray([float(f) for f in freqs])
+        lower = x.min()
+        upper = x.max()
+        normals = (x - lower) / (upper - lower)
+
+        return dict(zip(freqs, normals))
+
+    def poly_to_points(self, x1, poly):
+        x2 = range(int(max(x1)))
+        y2 = [poly(p) for p in x2]
+        return x2, y2
+
+    def add_forward_volts_plot(self):
+        title = 'Forward Volts'
+        labels = {'bottom':'Meter: Forward Watts', 'left':'Target: Forward Volts'}
+        plot = self.plot_layout.addPlot(title=title, labels=labels)
+        self.plot_added()
+        plot.showGrid(x=True, y=True)
+        plot.showButtons()
+        plot.addLegend()
+
+        colors = self.get_color_map(freqs)
+        for freq in freqs:
+            points = [p for p in self.data if p['freq'] == freq]
+            x = [p['m_fwd'] for p in points]
+            y = [p['t_fwd_volts'] for p in points]
+
+            poly = np.poly1d(np.polyfit(x, y, 2))
+            x2, y2 = self.poly_to_points(x, poly)
+
+            plot.plot(x2, y2, pen=pg.hsvColor(colors[freq]), name=freq)
+            plot.plot(x, y, pen=None, symbol='o', symbolSize=3, symbolPen=pg.hsvColor(colors[freq]))
+
+    def add_forward_watts_plot(self):
+        title = 'Forward Watts with Reference Line'
+        labels = {'bottom':'Meter: Forward Watts', 'left':'Target: Forward Watts'}
+        plot = self.plot_layout.addPlot(title=title, labels=labels)
+        self.plot_added()
+        plot.showGrid(x=True, y=True)
+        plot.showButtons()
+        plot.addLegend()
+
+        reference_points = list(range(0, 210, 10))
+        plot.plot(reference_points, reference_points, pen='w')
+        
+        colors = self.get_color_map(freqs)
+        for freq in freqs:
+            points = [p for p in self.data if p['freq'] == freq]
+            x = [p['m_fwd'] for p in points]
+            y = [p['t_fwd_watts'] for p in points]
+
+            poly = np.poly1d(np.polyfit(x, y, 2))
+            x2, y2 = self.poly_to_points(x, poly)
+
+            plot.plot(x2, y2, pen=pg.hsvColor(colors[freq]), name=freq)
+            plot.plot(x, y, pen=None, symbol='o', symbolSize=3, symbolPen=pg.hsvColor(colors[freq]))
+
+    def add_custom_plot(self, params):
+        title = 'Custom Plot'
+        labels = {'bottom':data_field_names[params['x']], 'left':data_field_names[params['y']]}
+        plot = self.plot_layout.addPlot(title=title, labels=labels)
+        plot.showGrid(x=True, y=True)
+        plot.showButtons()
+        plot.addLegend()
+
+        self.plot_added()
+
+        plot_freqs = params['freqs']
+        if 'none' in params['freqs']:
+            plot_freqs = freqs
+        colors = self.get_color_map(plot_freqs)
+
+        for freq in plot_freqs:
+            points = [p for p in self.data if p['freq'] == freq]
+            x = [p[params['x']] for p in points]
+            y = [p[params['y']] for p in points]
+
+            name = freq
+            if params['line']:
+                poly = np.poly1d(np.polyfit(x, y, 2))
+                x2, y2 = self.poly_to_points(x, poly)
+
+                plot.plot(x2, y2, pen=pg.hsvColor(colors[freq]), name=name)
+                name = None
+            if params['points']:
+                plot.plot(x, y, pen=None, symbol='o', symbolSize=3, symbolPen=pg.hsvColor(colors[freq]), name=name)
+                name = None
+
+    def reset_plot(self):
+        self.plot_layout.clear()
+        self.number_of_plots = 0
+
+    def plot_added(self):
+        self.number_of_plots += 1
+        if self.number_of_plots % 2 == 0:
+            self.plot_layout.nextRow()
 
     def draw_plots(self):
         freqs = self.freqs.selected_items()
-        plot_params = [plot.get_params() for plot in self.plots]
-        self.plot_layout.clear()
+        plot_params = [plot.get_params() for plot in self.custom_plots]
+        self.reset_plot()
+        
+        if self.fwd_v.isChecked():
+            self.add_forward_volts_plot()
+        if self.fwd_w.isChecked():
+            self.add_forward_watts_plot()
 
-        c = 0
         for params in plot_params:
             if params is None:
                 continue
 
-            plot_freqs = params[2]
-            if 'none' in params[2]:
-                plot_freqs = freqs
-
-            title = plot_freqs
-            labels = {'bottom':data_field_names[params[0]], 'left':data_field_names[params[1]]}
-            plot = self.plot_layout.addPlot(title=title, labels=labels)
-            plot.showGrid(x=True, y=True)
-            plot.showButtons()
-            plot.addLegend()
-            plot.setMenuEnabled(enableMenu=True, enableViewBoxMenu='same')
-
-            c += 1
-            if c % 2 == 0:
-                self.plot_layout.nextRow()
-
-            for freq in plot_freqs:
-                points = [p for p in self.data if p['freq'] == freq]
-                x = [p[params[0]] for p in points]
-                y = [p[params[1]] for p in points]
-                plot.plot(x, y, pen=pg.hsvColor(self.freq_colors[freq]), name=freq)
+            self.add_custom_plot(params)
 
     def set_data(self, data):
         self.data = data
@@ -406,8 +534,8 @@ class CalibrationApp(QWidget):
     def start_worker(self):
         self.worker.start(self.setup.get_script())
 
-    def worker_started(self):
-        self.progress.setMaximum(len(self.script['freqs']) * len(self.script['powers']))
+    def worker_started(self, steps):
+        self.progress.setMaximum(steps)
         self.progress.setValue(0)
         self.start.setEnabled(False)
         self.stop.setEnabled(True)
