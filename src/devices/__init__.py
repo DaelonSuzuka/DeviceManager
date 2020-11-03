2c9f619a1217d97a6a11ace98603b3730021b108
daelon <daelonsuzuka@gmail.com>
2020-11-03 10:30:01 -0500

    Moved CommonMessagesMixin to its own file

--- src/devices/__init__.py
+++ src/devices/__init__.py
@@ -1,10 +1,12 @@
-from .serial_device import SerialDevice, CommonMessagesMixin
+from .serial_device import SerialDevice
 from .remote_serial import RemoteSerial
 
 from .judi_filter import JudiFilter
 from .null_filter import NullFilter
 from .newline_filter import NewlineFilter
 from .delimiter_filter import DelimiterFilter
+
+from .judi_mixin import CommonMessagesMixin
 
 from .console_device import ConsoleDevice
 from .unknown_device import UnknownDevice, DeviceStates
