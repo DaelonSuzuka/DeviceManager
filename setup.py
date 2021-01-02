cdb4d4e1941ebc489fdbafb390ecacc28fe51712
DaelonSuzuka <david@ldgelectronics.com>
2021-01-01 21:09:33 -0500

    Updated cx_freeze script

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 import sys
 import cx_Freeze
+from pathlib import Path
+
 
 base = None
 if (sys.platform == "win32"):
     base = "Win32GUI" 
 
-
-exe = [
-    cx_Freeze.Executable(
-        "src/main.py", 
-        targetName='BenchControl', 
-        base=base,
-        shortcutName='BenchControl',
-        shortcutDir='DesktopFolder'
-        )
-]
 
 cx_Freeze.setup(
     name = "BenchControl",
@@ -24,32 +16,30 @@ cx_Freeze.setup(
             "packages": [
                 'serial',
                 'pyte',
+                'pyside2',
+                'qtawesome',
+                'qtpy',
+                'pyqtgraph',
+                'numpy',
+                'wcwidth',
+                'shiboken2',
             ],
             'excludes': [
                 'tkinter'
             ],
-            "include_files": [
-                'src/bundles.py',
-                'src/command_palette.py',
-                'src/device_controls.py',
-                'src/device_manager.py',
-                'src/device_widgets.py',
-                'src/diagnostics.py',
-                'src/log_monitor.py',
-                'src/main.py',
-                'src/main_window.py',
-                'src/manual_tuner.py ',
-                'src/remote_devices.py',
-                'src/serial_monitor.py',
-                'src/servitor.py',
-                'src/slickpicker.py',
-                'src/style.py',
-                'src/thread.py',
-                'src/tuner.py',
-                "src/devices",
-                "src/qt",
+            "include_files": [f.as_posix() for f in Path('src').rglob('*.py')],
+            "includes": [
+                "dataclasses",
             ]
         }
     },
-    executables = exe
+    executables = [
+        cx_Freeze.Executable(
+            "src/main.py", 
+            targetName='BenchControl', 
+            base=base,
+            shortcutName='BenchControl',
+            shortcutDir='DesktopFolder'
+            )
+    ]
 ) 
