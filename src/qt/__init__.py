c22cca8190d009e66109a7e2cb9a9e4905417090
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-01-07 12:04:10 -0500

    Removed unused widget files

--- src/qt/__init__.py
+++ src/qt/__init__.py
@@ -1,10 +1,4 @@
 from .qt import *
-
-from .flow_layout import *
-from .round_progress_bar import *
 from .utils import *
-
 from .layouts import *
 from .widgets import *
-from .qled import *
-from .gauge import *
