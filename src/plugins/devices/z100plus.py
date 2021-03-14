47844c6ed295962dbd0c4a476c9299b4c0a0fe05
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-03-14 13:04:02 -0400

    Added event handler to Z100Plus

--- src/plugins/devices/z100plus.py
+++ src/plugins/devices/z100plus.py
@@ -2,16 +2,8 @@ from devices import SerialDevice, JudiStandardMixin
 from qt import *
 
 
-# class Signals(QObject):
-#     antenna = Signal(int)
-
-#     @property
-#     def message_tree(self):
-#         return {
-#             "update": {
-#                 "antenna": self.antenna.emit
-#             }
-#         }
+class Signals(QObject):
+    event = Signal(dict)
 
 
 class Z100Plus(JudiStandardMixin, SerialDevice):
@@ -19,6 +11,12 @@ class Z100Plus(JudiStandardMixin, SerialDevice):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.signals = Signals()
-        # self.message_tree.merge(self.signals.message_tree)
+        self.signals = Signals()
         self.message_tree.merge(self.common_message_tree)
+        self.message_tree.merge({
+            "update": {
+                "event": self.event.emit
+            }
+        })
+
+        self.signals.event.connect(print)
