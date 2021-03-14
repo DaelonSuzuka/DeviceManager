e17d13a167dfff9efdcc6c346b49ee2a6f79a310
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-03-14 19:28:37 -0400

    Implemented collecting events in a queue

--- src/plugins/apps/tunermon/tunermon.py
+++ src/plugins/apps/tunermon/tunermon.py
@@ -2,6 +2,7 @@ from qt import *
 from devices import DeviceManager
 import appdirs
 from pathlib import Path
+from queue import Queue
 
 ex = {
     "event":"match_tested",
@@ -44,24 +45,37 @@ class TunerMonApp(QWidget):
         super().__init__(parent=parent)
         self.tab_name = 'Tuner Monitor'
         
-        db_path = appdirs.user_data_dir("Device Manager", "LDG Electronics")
-        Path(db_path).mkdir(parents=True, exist_ok=True)
-        db_name = db_path + '/tuner.db'
+        # db_path = appdirs.user_data_dir("Device Manager", "LDG Electronics")
+        # Path(db_path).mkdir(parents=True, exist_ok=True)
+        # db_name = db_path + '/tuner.db'
 
-        db = QSqlDatabase.addDatabase('QSQLITE', 'tuner')
-        db.setDatabaseName(db_name)
-        db.open()
+        # db = QSqlDatabase.addDatabase('QSQLITE', 'tuner')
+        # db.setDatabaseName(db_name)
+        # db.open()
 
         # db.exec_(initial_sql)
 
+        self.events = Queue()
+
         self.device_box = QComboBox(placeholderText="Select a device:")
-        self.connect = QPushButton("Connect")
+        self.connect = QPushButton("Connect", clicked=self.connect_pressed)
 
         with CHBoxLayout(self) as layout:
             with layout.vbox(1):
                 with layout.hbox():
                     layout.add(self.device_box, 1)
                     layout.add(self.connect)
+
+    def event_recieved(self, msg):
+        if msg['event'] == 'tune_start':
+            self.events.clear()
+
+        self.events.put(msg)
+
+    def connect_pressed(self):
+        guid = self.device_box.currentData()
+        if guid in self.devices:
+            self.devices[guid].signals.event.connect(self.event_recieved)
 
     def device_added(self, device):
         self.device_box.addItem(device.title, userData=device.guid)
